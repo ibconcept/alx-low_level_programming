5a792f71a33c916e76f08bd1ff77239605a5319e
william maina <willima09@gmail.com>
2023-10-18 10:10:25 -0700

    readme

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string to be appended
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+int i;
+int j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+
