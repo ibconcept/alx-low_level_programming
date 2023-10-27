7b04d103d614d329188a64922c02e922f5a989c5
william maina <willima09@gmail.com>
2023-10-27 16:52:34 -0700

    static lib

---
+++ 0x09-static_libraries/compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 0-isupper.o 0-isupper.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 100-atoi.o 100-atoi.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 1-strncat.o 1-strncat.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 2-strncpy.o 2-strncpy.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 3-strcmp.o 3-strcmp.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 4-strpbrk.o 4-strpbrk.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 9-strcpy.o 9-strcpy.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 0-memset.o 0-memset.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 1-isdigit.o 1-isdigit.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 2-strchr.o 2-strchr.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 3-islower.o 3-islower.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 3-strspn.o 3-strspn.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 5-strstr.o 5-strstr.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 0-strcat.o 0-strcat.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 1-memcpy.o 1-memcpy.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 2-strlen.o 2-strlen.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 3-puts.o 3-puts.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 4-isalpha.o 4-isalpha.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o 6-abs.o 6-abs.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c -o _putchar.o _putchar.c
+
