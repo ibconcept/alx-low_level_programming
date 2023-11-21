895e71af5f3feadf7b3402aaf450c218ddb0b968
william maina <willima09@gmail.com>
2023-11-21 12:24:54 -0800

    b

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,1 +1,35 @@
-#!/bin/bash
+#include "lists.h"
+
+/**
+ * find_listint_loop - Function to detect and find a loop in a linked list.
+ * @head: Pointer to the head of the linked list.
+ *
+ * Return: Address of the node where loop begins, or NULL if no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	if (!head)
+		return (NULL);
+
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+
+	return (NULL);
+}
