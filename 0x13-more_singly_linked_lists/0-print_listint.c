bf9cd7ae54f62bf1b0f4c5c6aefe1e612ba238e8
william maina <willima09@gmail.com>
2023-11-21 12:23:22 -0800

    b

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include "lists.h"
+
+/**
+ * print_listint - Function to display all elements in a linked list.
+ * @h: Pointer to the first node of the list (of type listint_t).
+ *
+ * Return: The total count of nodes in the linked list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num_nodes++;
+		h = h->next;
+	}
+	return (num_nodes);
+}
+
